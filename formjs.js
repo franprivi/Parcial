b3d7c0b64e235f7f7ed986690f26e05a56e69874
franprivi <skere.prividera@gmail.com>
2025-06-11 15:56:43 -0300

    update

--- formjs.js
+++ formjs.js
@@ -26,29 +26,13 @@ document.getElementById("contactoForm").addEventListener("submit", function(even
         document.getElementById("contactoForm").reset();
     }
 });
+const lightBtn = document.getElementById("lightModeBtn");
+const darkBtn = document.getElementById("darkModeBtn");
 
-document.addEventListener("DOMContentLoaded", function () {
-  const toggle = document.getElementById("themeToggle");
-  const html = document.documentElement;
-  const iconLuna = document.getElementById("icon-luna");
-  const iconSol = document.getElementById("icon-sol");
+lightBtn.addEventListener("click", () => {
+    document.body.classList.remove("dark-mode");
+});
 
-  // Restaurar tema guardado
-  const temaGuardado = localStorage.getItem("tema");
-  if (temaGuardado) {
-    html.setAttribute("data-bs-theme", temaGuardado);
-    iconLuna.style.display = temaGuardado === "dark" ? "none" : "inline";
-    iconSol.style.display = temaGuardado === "dark" ? "inline" : "none";
-  }
-
-  // Toggle modo oscuro
-  toggle.addEventListener("click", () => {
-    const actual = html.getAttribute("data-bs-theme") || "light";
-    const nuevo = actual === "light" ? "dark" : "light";
-    html.setAttribute("data-bs-theme", nuevo);
-    localStorage.setItem("tema", nuevo);
-
-    iconLuna.style.display = nuevo === "dark" ? "none" : "inline";
-    iconSol.style.display = nuevo === "dark" ? "inline" : "none";
-  });
+darkBtn.addEventListener("click", () => {
+    document.body.classList.add("dark-mode");
 });
